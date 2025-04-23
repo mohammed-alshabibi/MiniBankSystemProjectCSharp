2c4feaaae5e93f7412a53018d0596bddf8d97958
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-04-23 10:20:27 +0400

    create createAcount function

--- Program.cs
+++ Program.cs
@@ -2,9 +2,32 @@
 {
     internal class Program
     {
+        const string UserLogInFile = "login.txt";
+        static List<string> names = new List<string>();
+        static List<string> passwords = new List<string>();
+        static List<string> accountNumbers = new List<string>();
+        static List<double> balances = new List<double>();
+        static Queue<string> createAccountRequests = new Queue<string>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
+        //Function to create user account
+        public static void CreateAccount()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+            Console.WriteLine("Enter your account number:");
+            string accountNumber = Console.ReadLine();
+            Console.WriteLine("Enter your balance:");
+            double balance =double.Parse( Console.ReadLine());
+            string accountDetails = name+"|"+password + "|" + accountNumber + "|" + balance;
+            createAccountRequests.Enqueue(accountDetails);
+            Console.WriteLine("Account creation request sent to admin.");
+        }
+
+
     }
 }
